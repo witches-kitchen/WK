6e62a97c4a63ad47720ced7ec4ec7a1a2bdc8c2b
ExoPlant <lennyj2468+github@gmail.com>
2021-08-10 13:01:29 +0100

    Add my stuff I forgot to commit

--- src/main/java/cf/witcheskitchen/registry/WKBlocks.java
+++ src/main/java/cf/witcheskitchen/registry/WKBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
+import cf.witcheskitchen.WK;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
