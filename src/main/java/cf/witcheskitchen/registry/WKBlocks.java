7089e2a002c8bbfa2e6d35ae8efb0ca07271da5c
Sunconure11 <eyespot7b@aol.com>
2021-08-10 05:34:52 -0400

    Log blocks

--- src/main/java/cf/witcheskitchen/registry/WKBlocks.java
+++ src/main/java/cf/witcheskitchen/registry/WKBlocks.java
@@ -1,25 +1,29 @@
 package cf.witcheskitchen.registry;
 
+import cf.witcheskitchen.WK;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import cf.witcheskitchen.WK;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class WKBlocks
-{
+public class WKBlocks {
     static Block RAW_GINGERBREAD_BLOCK = new Block(FabricBlockSettings.of(Material.CAKE));
     static Block ELDER_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD));
     static Block SUMAC_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD));
     static Block HAWTHORN_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD));
     static Block BLACKTHORN_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD));
     static Block JUNIPER_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD));
-    public static void register()
-    {
+    static Block ELDER_LOG = new Block(FabricBlockSettings.of(Material.WOOD));
+    static Block SUMAC_LOG = new Block(FabricBlockSettings.of(Material.WOOD));
+    static Block HAWTHORN_LOG = new Block(FabricBlockSettings.of(Material.WOOD));
+    static Block BLACKTHORN_LOG = new Block(FabricBlockSettings.of(Material.WOOD));
+    static Block JUNIPER_LOG = new Block(FabricBlockSettings.of(Material.WOOD));
+
+    public static void register() {
         //Food blocks
         //Registry.register(Registry.BLOCK, new Identifier(WK.MODID, "raw_gingerbread_block"), RAW_GINGERBREAD_BLOCK);
         registerBlock("raw_gingerbread_block", RAW_GINGERBREAD_BLOCK, ItemGroup.BUILDING_BLOCKS);
@@ -30,9 +34,16 @@ public class WKBlocks
         registerBlock("hawthorn_planks", HAWTHORN_PLANKS, ItemGroup.BUILDING_BLOCKS);
         registerBlock("blackthorn_planks", BLACKTHORN_PLANKS, ItemGroup.BUILDING_BLOCKS);
         registerBlock("juniper_planks", JUNIPER_PLANKS, ItemGroup.BUILDING_BLOCKS);
+
+        //Wood logs
+        registerBlock("elder_log", ELDER_LOG, ItemGroup.BUILDING_BLOCKS);
+        registerBlock("sumac_log", SUMAC_LOG, ItemGroup.BUILDING_BLOCKS);
+        registerBlock("hawthorn_log", HAWTHORN_LOG, ItemGroup.BUILDING_BLOCKS);
+        registerBlock("blackthorn_log", BLACKTHORN_LOG, ItemGroup.BUILDING_BLOCKS);
+        registerBlock("juniper_log", JUNIPER_LOG, ItemGroup.BUILDING_BLOCKS);
     }
-    public static void registerBlock(String id, Block block, ItemGroup tab)
-    {
+
+    public static void registerBlock(String id, Block block, ItemGroup tab) {
         Registry.register(Registry.BLOCK, new Identifier(WK.MODID, id), block);
         Registry.register(Registry.ITEM, new Identifier(WK.MODID, id), new BlockItem(block, new FabricItemSettings().group(tab)));
     }
