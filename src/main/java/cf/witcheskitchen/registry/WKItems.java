b714190962dcc07da9585bb76778f9a06a5a5934
ExoPlant <lennyj2468+github@gmail.com>
2021-08-13 11:59:46 +0100

    Add Belladonna Seeds, plus a registerItem method

--- src/main/java/cf/witcheskitchen/registry/WKItems.java
+++ src/main/java/cf/witcheskitchen/registry/WKItems.java
@@ -1,9 +1,21 @@
 package cf.witcheskitchen.registry;
 
+import cf.witcheskitchen.WK;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class WKItems {
 
-    //FIXME: How do I register items?
-    static Item BELLADONNA_SEEDS;
+    static Item BELLADONNA_SEEDS = new Item(new FabricItemSettings().group(WK.OTHER_GROUP));
+    public static void register()
+    {
+        registerItem("belladonna_seeds", BELLADONNA_SEEDS);
+    }
+    public static void registerItem(String id, Item item)
+    {
+        Registry.register(Registry.ITEM, new Identifier(WK.MODID, id), item);
+    }
 }
+
