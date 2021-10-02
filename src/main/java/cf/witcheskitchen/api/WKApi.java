2ced6d2864e0d94e4e50263aae3f064411d20475
Sunconure11 <eyespot7b@aol.com>
2021-10-02 07:30:04 -0400

    Removed duplication in API

--- src/main/java/cf/witcheskitchen/api/WKApi.java
+++ src/main/java/cf/witcheskitchen/api/WKApi.java
@@ -6,6 +6,6 @@ import net.minecraft.entity.LivingEntity;
 public class WKApi {
 
     public static boolean isSpiritualEntity(LivingEntity entity) {
-        return entity.isUndead() || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.DEMONIC || entity.getGroup() == WKCreatureTypeEnum.ANGELIC || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.ELEMENTAL || entity.getGroup() == WKCreatureTypeEnum.ENIGMATIC || entity.getGroup() == EntityGroup.UNDEAD;
+        return entity.isUndead() || entity.getGroup() == WKCreatureTypeEnum.DEMONIC || entity.getGroup() == WKCreatureTypeEnum.ANGELIC || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.ELEMENTAL || entity.getGroup() == WKCreatureTypeEnum.ENIGMATIC || entity.getGroup() == EntityGroup.UNDEAD;
     }
 }
