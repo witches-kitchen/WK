3729ca9de99116a0463bbb984affa8fb3b27e145
Sunconure11 <eyespot7b@aol.com>
2021-10-08 14:18:06 -0400

    Update WKApi.java

--- src/main/java/cf/witcheskitchen/api/WKApi.java
+++ src/main/java/cf/witcheskitchen/api/WKApi.java
@@ -2,14 +2,11 @@ package cf.witcheskitchen.api;
 
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.BlazeEntity;
-import net.minecraft.entity.mob.EndermanEntity;
-import net.minecraft.entity.mob.GhastEntity;
-import net.minecraft.entity.mob.VexEntity;
+import net.minecraft.entity.mob.*;
 
 public class WKApi {
 
     public static boolean isSpiritualEntity(LivingEntity entity) {
-        return entity.isUndead() || entity.getGroup() == WKCreatureTypeEnum.DEMONIC || entity.getGroup() == WKCreatureTypeEnum.ANGELIC || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.ELEMENTAL || entity.getGroup() == WKCreatureTypeEnum.ENIGMATIC || entity.getGroup() == EntityGroup.UNDEAD || entity instanceof EndermanEntity || entity instanceof GhastEntity || entity instanceof BlazeEntity || entity instanceof VexEntity;
+        return entity.isUndead() || entity.getGroup() == WKCreatureTypeEnum.DEMONIC || entity.getGroup() == WKCreatureTypeEnum.ANGELIC || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.ELEMENTAL || entity.getGroup() == WKCreatureTypeEnum.ENIGMATIC || entity.getGroup() == EntityGroup.UNDEAD || entity instanceof EndermanEntity || entity instanceof GhastEntity || entity instanceof BlazeEntity || entity instanceof VexEntity || entity instanceof GuardianEntity || entity instanceof ElderGuardianEntity;
     }
 }
