cdb9a9f5699c7cdd46944779057620122d73e375
Sunconure11 <eyespot7b@aol.com>
2021-10-31 08:09:05 -0400

    Level 4 or higher drunkenness blinds you

--- src/main/java/cf/witcheskitchen/common/registry/WKFoodComponents.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKFoodComponents.java
@@ -6,6 +6,6 @@ import net.minecraft.item.FoodComponent;
 
 public class WKFoodComponents {
 
-    public static final FoodComponent FROSTING = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HUNGER), 0.66F).build();
-    public static final FoodComponent RUM_BALLS = new FoodComponent.Builder().hunger(4).saturationModifier(0.5f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(WKStatusEffects.DRUNK), 0.05F).build();
+    public static final FoodComponent FROSTING = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 250), 0.66F).build();
+    public static final FoodComponent RUM_BALLS = new FoodComponent.Builder().hunger(4).saturationModifier(0.5f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(WKStatusEffects.DRUNK, 400), 0.05F).build();
 }
--- src/main/java/cf/witcheskitchen/common/statuseffect/DrunkStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/DrunkStatusEffect.java
@@ -1,11 +1,26 @@
 package cf.witcheskitchen.common.statuseffect;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffects;
 
 //Todo: Learn shaders
 public class DrunkStatusEffect extends StatusEffect {
     public DrunkStatusEffect(StatusEffectType type, int color) {
         super(type, color);
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        if (amplifier >= 3) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 6000, 3));
+        }
     }
 }
