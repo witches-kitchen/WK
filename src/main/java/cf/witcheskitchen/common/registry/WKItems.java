c6cf96950ffc53a61d5830d89950cda1b23a21b3
Sunconure11 <eyespot7b@aol.com>
2021-10-31 07:58:35 -0400

    Rum is stored in the balls

--- src/main/java/cf/witcheskitchen/common/registry/WKItems.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKItems.java
@@ -22,10 +22,12 @@ public class WKItems {
     static Item WORMWOOD_SEEDS = new Item(new FabricItemSettings().group(WK.WK_GROUP));
 
     static Item DOLLOP_OF_FROSTING = new Item(new FabricItemSettings().food(WKFoodComponents.FROSTING).group(WK.WK_GROUP));
+    static Item CHOCOLATE_RUM_BALLS = new Item(new FabricItemSettings().food(WKFoodComponents.RUM_BALLS).group(WK.WK_GROUP));
 
     public static void register() {
         registerItem("belladonna_seeds", BELLADONNA_SEEDS);
         registerItem("dollop_of_frosting", DOLLOP_OF_FROSTING);
+        registerItem("chocolate_rum_balls", CHOCOLATE_RUM_BALLS);
     }
 
     public static void registerItem(String id, Item item) {
