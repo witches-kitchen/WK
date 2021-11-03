b8759d8ea6d7c16b50aebb0dcf4155181fc4f20f
Sunconure11 <eyespot7b@aol.com>
2021-11-03 18:07:50 -0400

    Maybe?

---
+++ src/main/java/cf/witcheskitchen/common/statuseffect/CooldownStatusEffect.java
@@ -0,0 +1,19 @@
+package cf.witcheskitchen.common.statuseffect;
+
+import cf.witcheskitchen.common.registry.WKStatusEffects;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+
+public class CooldownStatusEffect extends StatusEffect {
+    public CooldownStatusEffect(StatusEffectType type, int color) {
+        super(type, color);
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        if (entity.hasStatusEffect(WKStatusEffects.DISROBING)) {
+            entity.removeStatusEffect(WKStatusEffects.DISROBING);
+        }
+    }
+}
--- src/main/java/cf/witcheskitchen/common/statuseffect/DisrobingStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/DisrobingStatusEffect.java
@@ -1,9 +1,13 @@
 package cf.witcheskitchen.common.statuseffect;
 
+import cf.witcheskitchen.common.registry.WKStatusEffects;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffects;
 
 import java.util.Random;
 
@@ -32,22 +36,31 @@ public class DisrobingStatusEffect extends StatusEffect {
                 case 0 -> {
                     entity.dropItem(entity.getEquippedStack(EquipmentSlot.HEAD).getItem(), 1);
                     entity.getEquippedStack(EquipmentSlot.HEAD).decrement(1);
+                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
                 }
                 case 1 -> {
                     entity.dropItem(entity.getEquippedStack(EquipmentSlot.CHEST).getItem(), 1);
                     entity.getEquippedStack(EquipmentSlot.CHEST).decrement(1);
+                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
                 }
                 case 2 -> {
                     entity.dropItem(entity.getEquippedStack(EquipmentSlot.LEGS).getItem(), 1);
                     entity.getEquippedStack(EquipmentSlot.LEGS).decrement(1);
+                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
                 }
                 case 3 -> {
                     entity.dropItem(entity.getEquippedStack(EquipmentSlot.FEET).getItem(), 1);
                     entity.getEquippedStack(EquipmentSlot.FEET).decrement(1);
+                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
 
                 }
 
             }
         }
+    }
+
+    @Override
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        entity.addStatusEffect(new StatusEffectInstance(WKStatusEffects.COOLDOWN, 6000, 3));
     }
 }
