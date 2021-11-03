57d3cf8d0b071c3ca183113c0af05a14034975c8
Sunconure11 <eyespot7b@aol.com>
2021-11-03 16:29:48 -0400

    Update WKStatusEffects.java

--- src/main/java/cf/witcheskitchen/common/registry/WKStatusEffects.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKStatusEffects.java
@@ -18,7 +18,7 @@ public class WKStatusEffects {
     public static final StatusEffect FROSTBITE = create("frostbite", new FrostbiteStatusEffect(StatusEffectType.HARMFUL, 0xAFDBF5));
     public static final StatusEffect DRUNK = create("drunk", new DrunkStatusEffect(StatusEffectType.NEUTRAL, 0x7B3F00));
     public static final StatusEffect FIRE_SHIELD = create("fire_shield", new FireShieldStatusEffect(StatusEffectType.BENEFICIAL, 0xAF28500));
-    public static final StatusEffect DISROBING = create("disrobing", new DisrobingStatusEffect(StatusEffectType.HARMFUL, 0xAFDBF5));
+    public static final StatusEffect DISROBING = create("disrobing", new DisrobingStatusEffect(StatusEffectType.HARMFUL, 0xFDF5E6));
 
     private static <T extends StatusEffect> T create(String name, T effect) {
         STATUS_EFFECTS.put(effect, new Identifier(WK.MODID, name));
