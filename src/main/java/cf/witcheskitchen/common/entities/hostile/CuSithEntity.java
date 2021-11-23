197b398108c906dfa056f08ab61bada539f86719
Sunconure11 <eyespot7b@aol.com>
2021-11-23 14:51:02 -0500

    Cu-sith stuff

--- src/main/java/cf/witcheskitchen/common/entities/hostile/CuSithEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/hostile/CuSithEntity.java
@@ -1,14 +1,25 @@
 package cf.witcheskitchen.common.entities.hostile;
 
+import cf.witcheskitchen.api.WKCreatureTypeEnum;
+import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.Monster;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import java.util.SplittableRandom;
+
 public class CuSithEntity extends WKHostileEntity implements Monster, IAnimatable {
+
     public CuSithEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -21,5 +32,55 @@ public class CuSithEntity extends WKHostileEntity implements Monster, IAnimatabl
     @Override
     public AnimationFactory getFactory() {
         return null;
+    }
+
+    @Nullable
+    @Override
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+        SplittableRandom random = new SplittableRandom();
+        int var = random.nextInt(0, 7);
+        this.setVariant(var);
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(VARIANT, 0);
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putInt("Variant", this.getVariant());
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound tag) {
+        super.readCustomDataFromNbt(tag);
+        this.setVariant(tag.getInt("Variant"));
+    }
+
+    public int getVariant() {
+        return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 2);
+    }
+
+    public void setVariant(int variant) {
+        this.dataTracker.set(VARIANT, variant);
+    }
+
+    @Override
+    public int getVariants() {
+        return 2;
+    }
+
+    @Override
+    public boolean isUndead() {
+        return true;
+    }
+
+    @Override
+    public boolean canBreatheInWater() {
+        return true;
     }
 }
--- src/main/java/cf/witcheskitchen/common/entities/hostile/WKHostileEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/hostile/WKHostileEntity.java
@@ -1,12 +1,20 @@
 package cf.witcheskitchen.common.entities.hostile;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.Monster;
 import net.minecraft.world.World;
 
 public abstract class WKHostileEntity extends HostileEntity implements Monster {
+    public static final TrackedData<Integer> VARIANT = DataTracker.registerData(WKHostileEntity.class,
+            TrackedDataHandlerRegistry.INTEGER);
+
     public WKHostileEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
+
+    public abstract int getVariants();
 }
