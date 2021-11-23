922d9010f135892c074f7813d47f55e72fc599f9
Sunconure11 <eyespot7b@aol.com>
2021-11-23 15:01:44 -0500

    Update WKEntities.java

--- src/main/java/cf/witcheskitchen/common/registry/WKEntities.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKEntities.java
@@ -1,4 +1,18 @@
 package cf.witcheskitchen.common.registry;
 
+import cf.witcheskitchen.WK;
+import cf.witcheskitchen.common.entities.hostile.CuSithEntity;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class WKEntities {
+
+    public static final EntityType<CuSithEntity> CUSITH = Registry.register(Registry.ENTITY_TYPE,
+            new Identifier(WK.MODID, "cusith"), FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CuSithEntity::new)
+                    .dimensions(EntityDimensions.fixed(1.0F, 1.0F)).trackRangeBlocks(0).trackedUpdateRate(0).build());
+
 }
