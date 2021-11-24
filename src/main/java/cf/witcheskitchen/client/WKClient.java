7093ee07b380f0a0aa35032021c4dedd81e2c3f0
Sunconure11 <eyespot7b@aol.com>
2021-11-24 13:17:30 -0500

    Update WKClient.java

--- src/main/java/cf/witcheskitchen/client/WKClient.java
+++ src/main/java/cf/witcheskitchen/client/WKClient.java
@@ -4,10 +4,13 @@ import cf.witcheskitchen.client.render.CuSithRender;
 import cf.witcheskitchen.common.registry.WKBlocks;
 import cf.witcheskitchen.common.registry.WKEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
 
+@Environment(EnvType.CLIENT)
 public class WKClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
