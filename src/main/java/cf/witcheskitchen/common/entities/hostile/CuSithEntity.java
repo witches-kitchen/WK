ff7ff805e82d88eb584c30e34ed76ad031cc3026
Lyra <93697517+wallamide@users.noreply.github.com>
2021-11-27 06:23:40 -0600

    we have basic ai!
    need to add better animations to model for standing and attacking. need to fix the attribute settings. can target the player but current build cant handle dealing damage to player because the mob doesn't have any set damage amount.

--- src/main/java/cf/witcheskitchen/common/entities/hostile/CuSithEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/hostile/CuSithEntity.java
@@ -2,13 +2,15 @@ package cf.witcheskitchen.common.entities.hostile;
 
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.mob.Monster;
+//import net.minecraft.entity.mob.Monster;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.MathHelper;
@@ -39,6 +41,21 @@ public class CuSithEntity extends WKHostileEntity implements IAnimatable {
 
     public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 20).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.65);
+    }
+
+    //code for the below two groups inspired by or modified from grizzly bear mod, capybara mod, and adventurez mod
+   /* public static DefaultAttributeContainer.Builder createCuSithAttributes() {
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 55.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.85D)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0D).add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 2.2D).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 46.0D);
+    }*/
+
+    protected void initGoals() {
+        super.initGoals();
+        this.goalSelector.add(0, new EscapeDangerGoal(this, 2.0d));
+        this.goalSelector.add(1, new WanderAroundGoal(this, 0.85D));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 6.0f));
+        this.goalSelector.add(3, new FollowTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.add(1, (new FollowTargetGoal<>(this, PlayerEntity.class, true)).setMaxTimeWithoutVisibility(300));//from grizzly bear mod
     }
 
     @Override
