75dd2b497c350b46718855f0e07eadd2e9b4eb00
Sunconure11 <eyespot7b@aol.com>
2021-12-08 09:48:40 -0500

    Created a pillar block class for @wallamide

---
+++ src/main/java/cf/witcheskitchen/common/blocks/WKPillarBlock.java
@@ -0,0 +1,9 @@
+package cf.witcheskitchen.common.blocks;
+
+import net.minecraft.block.PillarBlock;
+
+public class WKPillarBlock extends PillarBlock {
+    public WKPillarBlock(Settings settings) {
+        super(settings);
+    }
+}
