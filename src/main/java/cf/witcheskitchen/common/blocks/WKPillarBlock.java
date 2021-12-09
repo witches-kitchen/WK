aeb1a529f3cd4551feb42933d95ef8047130394a
Sunconure11 <eyespot7b@aol.com>
2021-12-09 15:28:20 -0500

    Wallamide has stated that they don't need this

--- src/main/java/cf/witcheskitchen/common/blocks/WKPillarBlock.java
+++
@@ -1,9 +0,0 @@
-package cf.witcheskitchen.common.blocks;
-
-import net.minecraft.block.PillarBlock;
-
-public class WKPillarBlock extends PillarBlock {
-    public WKPillarBlock(Settings settings) {
-        super(settings);
-    }
-}
