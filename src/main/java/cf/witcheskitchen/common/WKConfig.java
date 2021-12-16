ac29da1256f1585c5bc6a42554fb49206f7a325f
Sunconure11 <eyespot7b@aol.com>
2021-12-16 03:50:33 -0500

    Update WKConfig.java

--- src/main/java/cf/witcheskitchen/common/WKConfig.java
+++ src/main/java/cf/witcheskitchen/common/WKConfig.java
@@ -4,8 +4,8 @@ import cf.witcheskitchen.WK;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 
+@Config(name = WK.MODID)
 public class WKConfig implements ConfigData {
-    @Config(name = WK.MODID)
     public class BWPConfig implements ConfigData {
 
     }
