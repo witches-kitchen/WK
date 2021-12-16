e7d8dff7dd1a8ebb6b665f64c817f429924d501e
Sunconure11 <eyespot7b@aol.com>
2021-12-16 03:48:52 -0500

    Example classes for the config and mod menu

---
+++ src/main/java/cf/witcheskitchen/client/compat/WKModMenu.java
@@ -0,0 +1,17 @@
+package cf.witcheskitchen.client.compat;
+
+import cf.witcheskitchen.common.WKConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+
+@Environment(EnvType.CLIENT)
+public class WKModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (ConfigScreenFactory<Screen>) screen -> AutoConfig.getConfigScreen(WKConfig.class, screen).get();
+    }
+}
---
+++ src/main/java/cf/witcheskitchen/common/WKConfig.java
@@ -0,0 +1,12 @@
+package cf.witcheskitchen.common;
+
+import cf.witcheskitchen.WK;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+public class WKConfig implements ConfigData {
+    @Config(name = WK.MODID)
+    public class BWPConfig implements ConfigData {
+
+    }
+}
