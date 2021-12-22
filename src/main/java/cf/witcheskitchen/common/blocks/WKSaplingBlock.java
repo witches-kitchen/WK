ca99021c22e0cba7dec1d09ebe143279217b2a3e
Lyra <93697517+wallamide@users.noreply.github.com>
2021-12-22 04:03:13 -0600

    Adds sapling block

---
+++ src/main/java/cf/witcheskitchen/common/blocks/WKSaplingBlock.java
@@ -0,0 +1,11 @@
+package cf.witcheskitchen.common.blocks;
+
+import net.minecraft.block.SaplingBlock;
+import net.minecraft.block.sapling.SaplingGenerator;
+
+public class WKSaplingBlock extends SaplingBlock {
+
+    public WKSaplingBlock(SaplingGenerator generator, Settings settings) {
+        super(generator, settings);
+    }
+}
