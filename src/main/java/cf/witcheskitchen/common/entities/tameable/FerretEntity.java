df5753b49af25ad54d6c0eb035a2c5a0584f4dad
Sunconure11 <eyespot7b@aol.com>
2021-12-29 10:02:20 -0500

    Gave ferrets a set of taming items

--- src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
@@ -1,17 +1,28 @@
 package cf.witcheskitchen.common.entities.tameable;
 
 import cf.witcheskitchen.api.WKTameableEntity;
+import com.google.common.collect.Sets;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Set;
 import java.util.UUID;
 
 public class FerretEntity extends WKTameableEntity {
+
+    public static final Set<Item> TAMING_INGREDIENTS;
+
+    static {
+        TAMING_INGREDIENTS = Sets.newHashSet(Items.RABBIT, Items.COOKED_RABBIT, Items.CHICKEN, Items.COOKED_CHICKEN, Items.EGG, Items.RABBIT_FOOT);
+    }
+
     public FerretEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
     }
