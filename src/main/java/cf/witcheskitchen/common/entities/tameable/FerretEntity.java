5d3e5fe57d7fae6b7dc952eaaa291f41a23ada1b
Sunconure11 <eyespot7b@aol.com>
2021-12-29 10:33:54 -0500

    Update FerretEntity.java

--- src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
@@ -2,9 +2,12 @@ package cf.witcheskitchen.common.entities.tameable;
 
 import cf.witcheskitchen.api.WKTameableEntity;
 import com.google.common.collect.Sets;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -24,6 +27,20 @@ public class FerretEntity extends WKTameableEntity {
 
     public FerretEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override //this adds basic ai
+    protected void initGoals() {
+        super.initGoals();
+        this.goalSelector.add(0, new SwimGoal(this)); //need to make this the ability to walk on the body of water's floor.
+        this.goalSelector.add(2, new LookAtEntityGoal(this, RabbitEntity.class, 12.0f));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, ChickenEntity.class, 12.0f));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 1, true));
+        this.goalSelector.add(4, new StopAndLookAtEntityGoal(this, MobEntity.class, 2.0f, 0.8f));
+        this.goalSelector.add(6, new WanderAroundFarGoal(this, 0.8D, 1));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, RabbitEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, ChickenEntity.class, true));
+        this.targetSelector.add(0, new RevengeGoal(this).setGroupRevenge());
     }
 
     @Override
