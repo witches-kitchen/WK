9787917fa6c3b0e7871b2465ffb33962b59cb0c8
Sunconure11 <eyespot7b@aol.com>
2021-12-29 10:58:46 -0500

    Took a cue from wolves on how to target specific mobs when untamed

--- src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
@@ -3,12 +3,10 @@ package cf.witcheskitchen.common.entities.tameable;
 import cf.witcheskitchen.api.WKTameableEntity;
 import com.google.common.collect.Sets;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.entity.passive.RabbitEntity;
-import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
@@ -23,13 +21,19 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class FerretEntity extends WKTameableEntity implements IAnimatable {
 
     public static final Set<Item> TAMING_INGREDIENTS;
+    public static final Predicate<LivingEntity> FOLLOW_TAMED_PREDICATE;
 
     static {
         TAMING_INGREDIENTS = Sets.newHashSet(Items.RABBIT, Items.COOKED_RABBIT, Items.CHICKEN, Items.COOKED_CHICKEN, Items.EGG, Items.RABBIT_FOOT);
+        FOLLOW_TAMED_PREDICATE = (entity) -> {
+            EntityType<?> entityType = entity.getType();
+            return entityType == EntityType.CHICKEN || entityType == EntityType.RABBIT;
+        };
     }
 
     private final AnimationFactory factory = new AnimationFactory(this);
@@ -47,8 +51,7 @@ public class FerretEntity extends WKTameableEntity implements IAnimatable {
         this.goalSelector.add(2, new MeleeAttackGoal(this, 1, true));
         this.goalSelector.add(4, new StopAndLookAtEntityGoal(this, MobEntity.class, 2.0f, 0.8f));
         this.goalSelector.add(6, new WanderAroundFarGoal(this, 0.8D, 1));
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, RabbitEntity.class, true));
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, ChickenEntity.class, true));
+        this.targetSelector.add(5, new UntamedActiveTargetGoal(this, AnimalEntity.class, true, FOLLOW_TAMED_PREDICATE));
         this.targetSelector.add(0, new RevengeGoal(this).setGroupRevenge());
     }
 
