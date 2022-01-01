563a97419e639ab28d3639900391b0d891134c16
Sunconure11 <eyespot7b@aol.com>
2022-01-01 14:41:34 -0500

    Expansion of the API for some stuff involving demons later down the road

--- src/main/java/cf/witcheskitchen/api/WKApi.java
+++ src/main/java/cf/witcheskitchen/api/WKApi.java
@@ -13,4 +13,18 @@ public class WKApi {
     public static boolean isSpiritualEntity(LivingEntity entity) {
         return entity.isUndead() || entity.getGroup() == WKCreatureTypeEnum.DEMONIC || entity.getGroup() == WKCreatureTypeEnum.ANGELIC || entity.getGroup() == WKCreatureTypeEnum.FAE || entity.getGroup() == WKCreatureTypeEnum.ELEMENTAL || entity.getGroup() == WKCreatureTypeEnum.ENIGMATIC || entity.getGroup() == EntityGroup.UNDEAD || entity instanceof EndermanEntity || entity instanceof GhastEntity || entity instanceof BlazeEntity || entity instanceof VexEntity || entity instanceof GuardianEntity || entity instanceof ElderGuardianEntity;
     }
+
+    /**
+     * This allows one to tell if something is a lesser demon, i.e not a boss.
+     */
+    public static boolean isLesserDemon() {
+        return false;
+    }
+
+    /**
+     * This allows one to tell if something is a greater demon, i.e something on the level of a boss.
+     */
+    public static boolean isGreaterDemon() {
+        return false;
+    }
 }
