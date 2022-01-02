fe660091350b8b6e82846d611288e80d60549416
Sunconure11 <eyespot7b@aol.com>
2022-01-02 12:41:41 -0500

    Ferrets flee cu-sith, as a test

--- src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/tameable/FerretEntity.java
@@ -3,6 +3,7 @@ package cf.witcheskitchen.common.entities.tameable;
 import cf.witcheskitchen.api.WKApi;
 import cf.witcheskitchen.api.WKCreatureTypeEnum;
 import cf.witcheskitchen.api.WKTameableEntity;
+import cf.witcheskitchen.common.entities.hostile.CuSithEntity;
 import cf.witcheskitchen.common.registry.WKEntities;
 import cf.witcheskitchen.common.registry.WKTags;
 import com.google.common.collect.Sets;
@@ -107,7 +108,7 @@ public class FerretEntity extends WKTameableEntity implements IAnimatable, IAnim
         this.goalSelector.add(2, new MeleeAttackGoal(this, 1, true));
         this.goalSelector.add(4, new StopAndLookAtEntityGoal(this, MobEntity.class, 2.0f, 0.8f));
         this.goalSelector.add(6, new WanderAroundFarGoal(this, 0.8D, 1));
-        //this.goalSelector.add(1, new FleeEntityGoal(this, WKApi.isGreaterDemon(), 16, 1, 3));
+        this.goalSelector.add(1, new FleeEntityGoal(this, CuSithEntity.class, 16, 1, 3));
         this.targetSelector.add(5, new UntamedActiveTargetGoal(this, AnimalEntity.class, true, FOLLOW_TAMED_PREDICATE));
         this.targetSelector.add(0, new RevengeGoal(this).setGroupRevenge());
     }
