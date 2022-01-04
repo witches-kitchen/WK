c860bd235636822245865297fcbfecf0c196d49b
Daniking <danikingrd@gmail.com>
2022-01-04 19:21:16 -0400

    Fix DisrobingStatusEffect

--- src/main/java/cf/witcheskitchen/common/registry/WKItems.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKItems.java
@@ -1,14 +1,33 @@
 package cf.witcheskitchen.common.registry;
 
 import cf.witcheskitchen.WK;
+import com.google.common.collect.Multimap;
+import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+import java.util.Map;
+import java.util.UUID;
 
 public class WKItems {
+
+    //PURELY FOR TESTING PURPOSES (TRINKETS)
+    //WILL BE REMOVED IN THE FUTURE
+    public static final Item DUMMY_CAPE = new TrinketItem(new FabricItemSettings().group(WK.WK_GROUP));
 
     static Item BELLADONNA_SEEDS = new Item(new FabricItemSettings().group(WK.WK_GROUP));
     static Item ST_JOHNS_WORT_SEEDS = new Item(new FabricItemSettings().group(WK.WK_GROUP));
@@ -29,6 +48,7 @@ public class WKItems {
     static Item CU_SITH_SPAWN_EGG = new SpawnEggItem(WKEntities.CUSITH, 3421236, 3497531, new Item.Settings().group(ItemGroup.MISC));
 
     public static void register() {
+        registerItem("dummy_cape", DUMMY_CAPE);
         registerItem("belladonna_seeds", BELLADONNA_SEEDS);
         registerItem("dollop_of_frosting", DOLLOP_OF_FROSTING);
         registerItem("chocolate_rum_balls", CHOCOLATE_RUM_BALLS);
--- src/main/java/cf/witcheskitchen/common/statuseffect/DisrobingStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/DisrobingStatusEffect.java
@@ -1,17 +1,30 @@
 package cf.witcheskitchen.common.statuseffect;
 
 import cf.witcheskitchen.common.registry.WKStatusEffects;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.effect.InstantStatusEffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Pair;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
-public class DisrobingStatusEffect extends StatusEffect {
+public class DisrobingStatusEffect extends InstantStatusEffect {
 
     public DisrobingStatusEffect(StatusEffectCategory type, int color) {
         super(type, color);
@@ -23,50 +36,67 @@ public class DisrobingStatusEffect extends StatusEffect {
     }
 
     @Override
-    public boolean canApplyUpdateEffect(int duration, int amplifier) {
-        return true;
-    }
-
-    @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        Random rand = entity.getRandom();
-        int i = rand.nextInt(100);
-        if (i < 75) {
-            switch (rand.nextInt(6)) {
-                case 0 -> {
-                    entity.dropItem(entity.getEquippedStack(EquipmentSlot.HEAD).getItem(), 1);
-                    entity.getEquippedStack(EquipmentSlot.HEAD).decrement(1);
-                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
+        final Random random = entity.getRandom();
+        final int i = random.nextInt(100) + 1;
+        if (i <= 50) {
+            final EquipmentSlot dice = getRandomArmor(random);
+            final ItemStack equippedArmor = entity.getEquippedStack(dice);
+            if (equippedArmor.isEmpty()) {
+                return;//fast fail, if there is no item in slot.
+            } else if (EnchantmentHelper.hasBindingCurse(equippedArmor)) {
+                return;//item has binding curse
+            } else if (EnchantmentHelper.hasVanishingCurse(equippedArmor)) {
+                return;//item should disappear on death.
+            }
+            if (entity.dropItem(equippedArmor.getItem(), 1) != null) {
+                equippedArmor.decrement(1);
+            }
+        } else {
+            final Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(entity);
+            if (component.isPresent()) {
+                //non empty slots
+                final List<Pair<SlotReference, ItemStack>> trinketInventory = component.get().getAllEquipped();
+                final int size = trinketInventory.size();
+                System.out.println(size);
+                if (trinketInventory.isEmpty()) {
+                    return;//all slots are empty
+                    //plus, we can get an exception from random
                 }
-                case 1 -> {
-                    entity.dropItem(entity.getEquippedStack(EquipmentSlot.CHEST).getItem(), 1);
-                    entity.getEquippedStack(EquipmentSlot.CHEST).decrement(1);
-                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
+                final int targetIndex = size == 1 ? 0 : random.nextInt(size) + 1;//if size is 1 there is only 1 equipped slot at index 0
+                //else generate a random number 1..= size
+                for (int slotIndex = 0; slotIndex < size; slotIndex++) {
+                    if (targetIndex == slotIndex) {
+                        final Pair<SlotReference, ItemStack> slotData = trinketInventory.get(slotIndex);
+                        final int index = slotData.getLeft().index();
+                        final Item itemInSlot = slotData.getRight().getItem();
+                        entity.dropItem(itemInSlot, 1);
+                        slotData.getLeft().inventory().getStack(index).decrement(1);
+                        break;
+                    }
                 }
-                case 2 -> {
-                    entity.dropItem(entity.getEquippedStack(EquipmentSlot.LEGS).getItem(), 1);
-                    entity.getEquippedStack(EquipmentSlot.LEGS).decrement(1);
-                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
-                }
-                case 3 -> {
-                    entity.dropItem(entity.getEquippedStack(EquipmentSlot.FEET).getItem(), 1);
-                    entity.getEquippedStack(EquipmentSlot.FEET).decrement(1);
-                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
-
-                }
-                //Todo: Fix trinket dupe bug when dropping items. Also make it target only one slot at a time.
-                case 4 -> {
-                    TrinketsApi.getTrinketComponent(entity).get().forEach((slotReference, itemStack) -> entity.dropItem(itemStack.getItem(), 1));
-                    entity.removeStatusEffect(WKStatusEffects.DISROBING);
-                }
-                case 5 -> entity.removeStatusEffect(WKStatusEffects.DISROBING);
-
             }
         }
     }
 
-    @Override
-    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
-        entity.addStatusEffect(new StatusEffectInstance(WKStatusEffects.COOLDOWN, 6000, 0));
+    private static EquipmentSlot getRandomArmor(final Random random) {
+        final int i = random.nextInt(4);
+        switch (i) {
+            case 0 -> {
+                return EquipmentSlot.HEAD;
+            }
+            case 1 -> {
+                return EquipmentSlot.CHEST;
+            }
+            case 2 -> {
+                return EquipmentSlot.LEGS;
+            }
+            case 3 -> {
+                return EquipmentSlot.FEET;
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
