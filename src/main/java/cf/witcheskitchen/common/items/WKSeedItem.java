daf6fc5ae2292adb102417707b7286f3d27755c8
Lyra <93697517+wallamide@users.noreply.github.com>
2022-01-04 10:53:52 -0600

    adds seed and sapling item information

---
+++ src/main/java/cf/witcheskitchen/common/items/WKSeedItem.java
@@ -0,0 +1,66 @@
+package cf.witcheskitchen.common.items;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.jetbrains.annotations.Nullable;
+
+import cf.witcheskitchen.common.blocks.WKCropBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome.Category;
+
+public class WKSeedItem extends AliasedBlockItem {
+
+    private List<Category> category;
+
+    public WKSeedItem(Block block, Settings settings, List<Category> category) {
+        super(block, settings);
+        ((WKCropBlock) block).setSeedsItem(this);
+        this.category = category;
+    }
+
+    public WKSeedItem(Block block, Settings settings, Category... category) {
+        this(block, settings, List.of(category));
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        BlockPos hitPos = context.getBlockPos();
+        World world = context.getWorld();
+        BlockState state = world.getBlockState(hitPos);
+        if (state.getBlock() instanceof FarmlandBlock && context.getSide() == Direction.UP) {
+            return super.useOnBlock(context);
+        }
+        return ActionResult.FAIL;
+    }
+
+    /*@Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("info.croptopia.seed").setStyle(Style.EMPTY.withColor(Formatting.GRAY))
+                .append(" ").append(category.get(0).asString().toLowerCase(Locale.ROOT)));
+    }*/
+
+    @Deprecated(forRemoval = true)
+    public void setCategory(List<Category> category) {
+        this.category = category;
+    }
+
+    @Deprecated(forRemoval = true)
+    public List<Category> getCategory() {
+        return category;
+    }
+}
