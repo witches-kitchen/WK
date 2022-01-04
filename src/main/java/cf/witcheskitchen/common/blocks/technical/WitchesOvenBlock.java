2959d4cf07f050878d02c69b5112f00dd6f06773
Daniking <danikingrd@gmail.com>
2022-01-04 08:38:13 -0400

    Fix witches oven model, texture and voxelshape

--- src/main/java/cf/witcheskitchen/common/blocks/technical/WitchesOvenBlock.java
+++ src/main/java/cf/witcheskitchen/common/blocks/technical/WitchesOvenBlock.java
@@ -1,9 +1,7 @@
 package cf.witcheskitchen.common.blocks.technical;
 
 import cf.witcheskitchen.common.blocks.entity.WitchesOvenBlockEntity;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
@@ -12,6 +10,9 @@ import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
 
 public class WitchesOvenBlock extends WKDeviceBlock {
@@ -19,9 +20,29 @@ public class WitchesOvenBlock extends WKDeviceBlock {
     public static final BooleanProperty LIT = Properties.LIT;
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
 
+    public static final VoxelShape SHAPE = VoxelShapes.union(
+            createCuboidShape(0, 14, 0, 16, 16,16),
+            createCuboidShape(0, 2, 1, 16, 13, 16),
+            createCuboidShape(0, 11, 0, 16, 13, 1),
+            createCuboidShape(13, 2, 0, 16, 4, 1),
+            createCuboidShape(0, 2, 0, 3, 4, 1),
+            createCuboidShape(3, 2, 0,13, 8, 1),
+            createCuboidShape(5, 8, 0, 11, 9, 1),
+            createCuboidShape(1, 13, 2, 15, 14,15),
+            createCuboidShape(13, 0, 1, 15, 2, 3),
+            createCuboidShape(13, 0, 13, 15, 2, 15),
+            createCuboidShape(1, 0, 13, 3, 2, 15),
+            createCuboidShape(1, 0, 1, 3, 2, 3)
+    );
+
     public WitchesOvenBlock(Settings settings) {
         super(settings);
         this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH).with(LIT, false));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     @Override
