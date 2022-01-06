e77d99456847bec3b0e02314f284b3961a1c3a0e
Sunconure11 <eyespot7b@aol.com>
2022-01-05 19:14:14 -0500

    Taglock blacklist, for when the time comes

--- src/main/java/cf/witcheskitchen/common/registry/WKTags.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKTags.java
@@ -10,4 +10,5 @@ public class WKTags {
     public static final Tag<EntityType<?>> GREATER_DEMON = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "greater_demon"));
     public static final Tag<EntityType<?>> LESSER_DEMON = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "lesser_demon"));
     public static final Tag<EntityType<?>> GHOST = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "ghost"));
+    public static final Tag<EntityType<?>> TAGLOCK_BLACKLIST = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "taglock_blacklist"));
 }
