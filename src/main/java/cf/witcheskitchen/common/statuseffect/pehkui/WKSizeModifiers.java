c15aadbb0b3df06b3b4138a4810e39f796169178
Sunconure11 <eyespot7b@aol.com>
2022-01-07 16:09:57 -0500

    Update WKSizeModifiers.java

--- src/main/java/cf/witcheskitchen/common/statuseffect/pehkui/WKSizeModifiers.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/pehkui/WKSizeModifiers.java
@@ -10,5 +10,6 @@ public class WKSizeModifiers {
     public static final ScaleType LILLIPUTIAN = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("lilliputian"), ScaleType.Builder.create().build());
 
     public static final ScaleType CAT_SITH_SIZE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("cat_sith_size"), ScaleType.Builder.create().build());
-    public static final ScaleType LYCANTHROPE_SIZE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("lycanthrope_size"), ScaleType.Builder.create().build());
+    public static final ScaleType LYCANTHROPE_HUMANOID_SIZE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("lycanthrope_humanoid_size"), ScaleType.Builder.create().build());
+    public static final ScaleType LYCANTHROPE_WOLF_SIZE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("lycanthrope_wolf_size"), ScaleType.Builder.create().build());
 }
