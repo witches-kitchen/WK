c728ab0771cc522d450a10f61c13c456dce7d1c7
Sunconure11 <eyespot7b@aol.com>
2022-01-07 16:43:12 -0500

    Stuff

--- src/main/java/cf/witcheskitchen/common/statuseffect/pehkui/WKSizeModifiers.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/pehkui/WKSizeModifiers.java
@@ -5,8 +5,6 @@ import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleType;
 
 public class WKSizeModifiers {
-
-    public static final ScaleType PANTAGRUELIAN = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("pantagruelian"), ScaleType.Builder.create().build());
     public static final ScaleType LILLIPUTIAN = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("lilliputian"), ScaleType.Builder.create().build());
 
     public static final ScaleType CAT_SITH_SIZE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, WK.id("cat_sith_size"), ScaleType.Builder.create().build());
