1ffd25f812a7119929678bd17139393b0be053f5
Sunconure11 <eyespot7b@aol.com>
2022-01-07 18:14:47 -0500

    These could be done in a day, but I wanna go off soon

---
+++ src/main/java/cf/witcheskitchen/common/statuseffect/ThickSkinStatusEffect.java
@@ -0,0 +1,10 @@
+package cf.witcheskitchen.common.statuseffect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class ThickSkinStatusEffect extends StatusEffect {
+    public ThickSkinStatusEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
