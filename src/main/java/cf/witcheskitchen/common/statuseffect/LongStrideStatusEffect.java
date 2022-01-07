7ceb2683353b34de9a68b80b2de99eaf13eeba56
Sunconure11 <eyespot7b@aol.com>
2022-01-07 18:07:33 -0500

    Creating the starts of some new potions
    Long Reach - Reach longer
    Long Stride - Climb up more than one block at a time
    Frost Shield - Similar in concept to Fire Shield, but based around frost
    
    The other ones on the list will be started after these are done.

---
+++ src/main/java/cf/witcheskitchen/common/statuseffect/LongStrideStatusEffect.java
@@ -0,0 +1,10 @@
+package cf.witcheskitchen.common.statuseffect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class LongStrideStatusEffect extends StatusEffect {
+    public LongStrideStatusEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
