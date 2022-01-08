b886fae3c36f3b048ea2a1bf391b7249290e23ff
Sunconure11 <eyespot7b@aol.com>
2022-01-08 08:01:28 -0500

    Update ShrinkingStatusEffect.java

--- src/main/java/cf/witcheskitchen/common/statuseffect/ShrinkingStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/ShrinkingStatusEffect.java
@@ -3,6 +3,7 @@ package cf.witcheskitchen.common.statuseffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
+//use pehkui here
 public class ShrinkingStatusEffect extends StatusEffect {
     public ShrinkingStatusEffect(StatusEffectCategory category, int color) {
         super(category, color);
