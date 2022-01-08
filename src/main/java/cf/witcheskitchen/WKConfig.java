d5d4123939ab9f89b1939f59d99eaf59c15e4451
Sunconure11 <eyespot7b@aol.com>
2022-01-08 15:45:18 -0500

    Shell of a config file. Will add to this once @wallamide finishes cleaning up ferrets

---
+++ src/main/java/cf/witcheskitchen/WKConfig.java
@@ -0,0 +1,8 @@
+package cf.witcheskitchen;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = WK.MODID)
+public class WKConfig implements ConfigData {
+}
