72284fd2c71afe94f89ec658d71e4c8e5ebbb743
Sunconure11 <eyespot7b@aol.com>
2022-01-08 05:05:00 -0500

    Update PhasingStatusEffect.java

--- src/main/java/cf/witcheskitchen/common/statuseffect/PhasingStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/PhasingStatusEffect.java
@@ -1,6 +1,5 @@
 package cf.witcheskitchen.common.statuseffect;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.InstantStatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
@@ -12,7 +11,6 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public class PhasingStatusEffect extends InstantStatusEffect {
     public PhasingStatusEffect(StatusEffectCategory category, int color) {
@@ -25,36 +23,31 @@ public class PhasingStatusEffect extends InstantStatusEffect {
     }
 
     @Override
-    public boolean isInstant() {
-        return true;
-    }
-
-    @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
     }
 
     @Override
-    public void applyInstantEffect(@Nullable Entity source, @Nullable Entity attacker, LivingEntity target, int amplifier, double proximity) {
-        BlockPos pos = target.getBlockPos();
-        World world = target.getEntityWorld();
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        BlockPos pos = entity.getBlockPos();
+        World world = entity.getEntityWorld();
         if (!world.isClient) {
-            double d = target.getX();
-            double e = target.getY();
-            double f = target.getZ();
+            double d = entity.getX();
+            double e = entity.getY();
+            double f = entity.getZ();
 
             for (int i = 0; i < 16; ++i) {
-                double g = target.getX() + (target.getRandom().nextDouble() - 0.5D) * 16.0D;
-                double h = MathHelper.clamp(target.getY() + (double) (target.getRandom().nextInt(16) - 8), world.getBottomY(), world.getBottomY() + ((ServerWorld) world).getLogicalHeight() - 1);
-                double j = target.getZ() + (target.getRandom().nextDouble() - 0.5D) * 16.0D;
-                if (target.hasVehicle()) {
-                    target.stopRiding();
+                double g = entity.getX() + (entity.getRandom().nextDouble() - 0.5D) * 16.0D;
+                double h = MathHelper.clamp(entity.getY() + (double) (entity.getRandom().nextInt(16) - 8), world.getBottomY(), world.getBottomY() + ((ServerWorld) world).getLogicalHeight() - 1);
+                double j = entity.getZ() + (entity.getRandom().nextDouble() - 0.5D) * 16.0D;
+                if (entity.hasVehicle()) {
+                    entity.stopRiding();
                 }
 
-                if (target.teleport(g, h, j, true)) {
-                    SoundEvent soundEvent = target instanceof FoxEntity ? SoundEvents.ENTITY_FOX_TELEPORT : SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT;
+                if (entity.teleport(g, h, j, true)) {
+                    SoundEvent soundEvent = entity instanceof FoxEntity ? SoundEvents.ENTITY_FOX_TELEPORT : SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT;
                     world.playSound(null, d, e, f, soundEvent, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                    target.playSound(soundEvent, 1.0F, 1.0F);
+                    entity.playSound(soundEvent, 1.0F, 1.0F);
                     break;
                 }
             }
