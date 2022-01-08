df999b737c1c1cd806bd1e025d06fb9f4b30fced
Sunconure11 <eyespot7b@aol.com>
2022-01-08 05:35:34 -0500

    Partial work on the shadows potion

--- src/main/java/cf/witcheskitchen/common/statuseffect/ShadowsStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/ShadowsStatusEffect.java
@@ -1,10 +1,35 @@
 package cf.witcheskitchen.common.statuseffect;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.util.math.BlockPos;
 
 public class ShadowsStatusEffect extends StatusEffect {
     public ShadowsStatusEffect(StatusEffectCategory category, int color) {
         super(category, color);
+    }
+
+    @Override
+    public boolean isBeneficial() {
+        return true;
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
+
+    //Todo: Find methods to call for telling if an entity is moving. Also find values for average cave light level, and possibly create a mixin for making armor invisible.
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+       // BlockPos pos = entity.getBlockPos();
+        //if (entity.getEntityWorld().isNight() || entity.getEntityWorld().getLightLevel(pos, )) {
+       //     entity.setInvisible(true);
+       // }
+        //if (entity.getEntityWorld().isDay() || entity.getEntityWorld().getLightLevel(pos, )) {
+        // entity.setInvisible(true);
+       // }
     }
 }
