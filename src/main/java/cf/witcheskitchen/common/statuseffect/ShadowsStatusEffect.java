f0672a2952d0fe7f210be292b38cde9743c48002
Sunconure11 <eyespot7b@aol.com>
2022-01-08 06:05:11 -0500

    Partially functioning shadows potion. Needs testing.
    @wallamide @DanikingRD

--- src/main/java/cf/witcheskitchen/common/statuseffect/ShadowsStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/ShadowsStatusEffect.java
@@ -1,6 +1,7 @@
 package cf.witcheskitchen.common.statuseffect;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.util.math.BlockPos;
@@ -24,12 +25,11 @@ public class ShadowsStatusEffect extends StatusEffect {
     //Todo: Find methods to call for telling if an entity is moving. Also find values for average cave light level, and possibly create a mixin for making armor invisible.
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-       // BlockPos pos = entity.getBlockPos();
-        //if (entity.getEntityWorld().isNight() || entity.getEntityWorld().getLightLevel(pos, )) {
-       //     entity.setInvisible(true);
-       // }
-        //if (entity.getEntityWorld().isDay() || entity.getEntityWorld().getLightLevel(pos, )) {
-        // entity.setInvisible(true);
-       // }
+        BlockPos pos = entity.getBlockPos();
+        if (entity.getEntityWorld().isNight() || !entity.getEntityWorld().isSkyVisible(pos)) {
+            entity.setInvisible(true);
+        } else if (entity.getEntityWorld().isDay() && entity.getEntityWorld().isSkyVisible(pos) && MovementType.PLAYER.equals(false)) {
+            entity.setInvisible(true);
+        }
     }
 }
