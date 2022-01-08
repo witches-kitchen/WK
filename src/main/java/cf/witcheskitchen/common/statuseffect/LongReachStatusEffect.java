036cb4b1c20a33f9f251389b494c7c1755c47e67
Sunconure11 <eyespot7b@aol.com>
2022-01-08 07:58:24 -0500

    Notes

--- src/main/java/cf/witcheskitchen/common/statuseffect/LongReachStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/LongReachStatusEffect.java
@@ -3,6 +3,8 @@ package cf.witcheskitchen.common.statuseffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
+
+//use attribute reach dependency here
 public class LongReachStatusEffect extends StatusEffect {
     public LongReachStatusEffect(StatusEffectCategory category, int color) {
         super(category, color);
--- src/main/java/cf/witcheskitchen/common/statuseffect/LongStrideStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/LongStrideStatusEffect.java
@@ -3,6 +3,8 @@ package cf.witcheskitchen.common.statuseffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
+
+//use step-height dependency here
 public class LongStrideStatusEffect extends StatusEffect {
     public LongStrideStatusEffect(StatusEffectCategory category, int color) {
         super(category, color);
