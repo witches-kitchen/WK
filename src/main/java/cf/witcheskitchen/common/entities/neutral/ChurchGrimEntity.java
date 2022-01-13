dcf427d0f9e8e585b7e298ad8871bfe04c2d0361
Sunconure11 <eyespot7b@aol.com>
2022-01-13 16:47:44 -0500

    Update ChurchGrimEntity.java

--- src/main/java/cf/witcheskitchen/common/entities/neutral/ChurchGrimEntity.java
+++ src/main/java/cf/witcheskitchen/common/entities/neutral/ChurchGrimEntity.java
@@ -2,13 +2,17 @@ package cf.witcheskitchen.common.entities.neutral;
 
 import cf.witcheskitchen.api.WKHostileEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class ChurchGrimEntity extends WKHostileEntity implements IAnimatable {
+import java.util.UUID;
+
+public class ChurchGrimEntity extends WKHostileEntity implements IAnimatable, Angerable {
 
     //Add a string or something here for a variant that is a white, short-haired dog and can appear if named Max
     public ChurchGrimEntity(EntityType<? extends HostileEntity> entityType, World world) {
@@ -28,5 +32,31 @@ public class ChurchGrimEntity extends WKHostileEntity implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return null;
+    }
+
+    @Override
+    public int getAngerTime() {
+        return 0;
+    }
+
+    @Override
+    public void setAngerTime(int ticks) {
+
+    }
+
+    @Nullable
+    @Override
+    public UUID getAngryAt() {
+        return null;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID uuid) {
+
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+
     }
 }
