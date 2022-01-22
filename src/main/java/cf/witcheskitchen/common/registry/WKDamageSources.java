578b8bfb439793be968420d6a370a5fe0d078edd
Sunconure11 <eyespot7b@aol.com>
2022-01-22 11:54:16 -0500

    Update WKDamageSources.java

--- src/main/java/cf/witcheskitchen/common/registry/WKDamageSources.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKDamageSources.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.damage.DamageSource;
 public class WKDamageSources {
 
     public static final DamageSource ON_OVEN = new WKFireDamageSource("on_oven");
+    public static final DamageSource HOLY = new WKHolyDamageSource("holy");
 
     private static class WKFireDamageSource extends DamageSource {
 
@@ -14,4 +15,15 @@ public class WKDamageSources {
             this.setFire();
         }
     }
-}
+
+        private static class WKHolyDamageSource extends DamageSource {
+
+            protected WKHolyDamageSource(String name) {
+                super(name);
+                this.setBypassesArmor();
+                this.setFire();
+                this.isMagic();
+                this.setUnblockable();
+            }
+        }
+    }
