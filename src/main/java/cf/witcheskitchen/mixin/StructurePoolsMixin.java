c4cce436ee9134224631f77b782a1be1dd243926
Sunconure11 <eyespot7b@aol.com>
2022-02-01 15:53:44 -0500

    Added a fixme on a mixin to point out an error

--- src/main/java/cf/witcheskitchen/mixin/StructurePoolsMixin.java
+++ src/main/java/cf/witcheskitchen/mixin/StructurePoolsMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class StructurePoolsMixin {
 
 
+    //FIXME: Mixin Error!
     @Inject(method = "register", at = {@At("HEAD")}, cancellable = true)
     private static void register(StructurePool templatePool, CallbackInfoReturnable<StructurePool> cir) {
         //System.out.println(templatePool.getId());
