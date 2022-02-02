62ea2346b50b368c0ed1df8cf91465a0d748cf55
Daniking <danikingrd@gmail.com>
2022-02-02 17:15:09 -0400

    Small reformat on sounds registry

--- src/main/java/cf/witcheskitchen/common/registry/WKSounds.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKSounds.java
@@ -9,27 +9,26 @@ import net.minecraft.util.registry.Registry;
 public class WKSounds {
 
     // Cu-Sith 
-    public static SoundEvent CUSITH_IDLE_EVENT = register("witcheskitchen:cusith_ambient");
-    public static SoundEvent CUSITH_DEATH_EVENT = register("witcheskitchen:cusith_death");
-    public static SoundEvent CUSITH_HOWL_EVENT = register("witcheskitchen:cusith_howl");
-
+    public static final SoundEvent CUSITH_IDLE_EVENT = register("cusith_ambient");
+    public static final SoundEvent CUSITH_DEATH_EVENT = register("cusith_death");
+    public static final SoundEvent CUSITH_HOWL_EVENT = register("cusith_howl");
+ 
     //Ferret
-    public static SoundEvent FERRET_IDLE_EVENT = register("witcheskitchen:ferret_ambient");
-    public static SoundEvent FERRET_ATTACK_EVENT = register("witcheskitchen:ferret_attack");
-    public static SoundEvent FERRET_CHIRP_EVENT = register("witcheskitchen:ferret_chirp");
+    public static final SoundEvent FERRET_IDLE_EVENT = register("ferret_ambient");
+    public static final SoundEvent FERRET_ATTACK_EVENT = register("ferret_attack");
+    public static final SoundEvent FERRET_CHIRP_EVENT = register("ferret_chirp");
 
     //Cauldron
-    public static SoundEvent CAULDRON_BOIL_EVENT = register("witcheskitchen:cauldron_bubble");
+    public static final SoundEvent CAULDRON_BOIL_EVENT = register("cauldron_bubble");
 
     //Broom
-    public static SoundEvent BROOM_RIDING_EVENT = register("witcheskitchen:broom_loop");
-    public static SoundEvent BROOM_USE_EVENT = register("witcheskitchen:broom_mount1");
+    public static final SoundEvent BROOM_RIDING_EVENT = register("broom_loop");
+    public static final SoundEvent BROOM_USE_EVENT = register("broom_mount1");
 
     private static SoundEvent register(String id) {
-        return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(new Identifier(id)));
+        return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(new Identifier(WK.MODID, id)));
     }
-
-
+    
     public static void register() {
         if (WKConfig.get().debugMode) {
             WK.logger.info("Witches Kitchen Base Sounds: Successfully Loaded");
