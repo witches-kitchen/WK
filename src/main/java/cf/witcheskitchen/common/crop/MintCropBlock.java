8104e33324a18b60221a35beba841942f1890086
Sunconure11 <eyespot7b@aol.com>
2022-02-06 19:58:02 -0500

    Fix formatting issue, start of mint

---
+++ src/main/java/cf/witcheskitchen/common/crop/MintCropBlock.java
@@ -0,0 +1,47 @@
+package cf.witcheskitchen.common.crop;
+
+import cf.witcheskitchen.api.WKCropBlock;
+import cf.witcheskitchen.common.registry.WKItems;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.state.property.IntProperty;
+
+import static cf.witcheskitchen.common.crop.MintCropBlock.Type.COMMON;
+
+//Fixme: Figure out why it wants to call stuff from other crops. This issue plagued amaranth.
+public class MintCropBlock extends WKCropBlock {
+    public static final int MAX_AGE = 5;
+    private final Type type;
+
+    public MintCropBlock(Settings settings) {
+        super(settings, COMMON);
+    }
+
+    public MintCropBlock(Settings settings, Type rarity) {
+        super(settings);
+        this.type = rarity;
+    }
+
+    @Override
+    public IntProperty getAgeRange() {
+        return IntProperty.of("age", 0, MAX_AGE);
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    protected ItemConvertible getSeedsItem() {
+        return switch (this.type) {
+            case COMMON -> WKItems.WORMWOOD_SEEDS;
+        };
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    public enum Type {
+        COMMON
+    }
+}
