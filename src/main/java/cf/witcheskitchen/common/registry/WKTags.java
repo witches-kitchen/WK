c9d1e829fe6aef1a4634bf724b05845423c61621
Sunconure11 <eyespot7b@aol.com>
2022-02-28 11:43:36 -0500

    Partial tag functionality

--- src/main/java/cf/witcheskitchen/common/registry/WKTags.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKTags.java
@@ -5,31 +5,36 @@ import cf.witcheskitchen.WKConfig;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class WKTags {
-    public static final Tag<EntityType<?>> GREATER_DEMON = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "greater_demon"));
-    public static final Tag<EntityType<?>> LESSER_DEMON = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "lesser_demon"));
-    public static final Tag<EntityType<?>> GHOST = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "ghost"));
-    public static final Tag<EntityType<?>> TAGLOCK_BLACKLIST = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "taglock_blacklist"));
-    public static final Tag<EntityType<?>> COLD_IRON_WEAK = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "cold_iron_weak"));
-    public static final Tag<EntityType<?>> COLD_IRON_IMMUNE = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "cold_iron_immune"));
-    public static final Tag<EntityType<?>> SILVER_WEAK = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "silver_iron_weak"));
-    public static final Tag<EntityType<?>> SILVER_IMMUNE = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "silver_iron_immune"));
-    public static final Tag<EntityType<?>> RIGHT_HAND_WITCH_SUMMON = TagFactory.ENTITY_TYPE.create(new Identifier(WK.MODID, "right_hand_witch_summon"));
+    public static final TagKey<EntityType<?>> GREATER_DEMON = registerEntity("greater_demon");
+    public static final TagKey<EntityType<?>> LESSER_DEMON = registerEntity("lesser_demon");
+    public static final TagKey<EntityType<?>> GHOST = registerEntity("ghost");
+    public static final TagKey<EntityType<?>> TAGLOCK_BLACKLIST = registerEntity("taglock_blacklist");
+    public static final TagKey<EntityType<?>> COLD_IRON_WEAK = registerEntity("cold_iron_weak");
+    public static final TagKey<EntityType<?>> COLD_IRON_IMMUNE = registerEntity("cold_iron_immune");
+    public static final TagKey<EntityType<?>> SILVER_WEAK = registerEntity("silver_iron_weak");
+    public static final TagKey<EntityType<?>> SILVER_IMMUNE = registerEntity("silver_iron_immune");
+    public static final TagKey<EntityType<?>> RIGHT_HAND_WITCH_SUMMON = registerEntity("right_hand_witch_summon");
     public static final TagKey<EntityType<?>> LEFT_HAND_WITCH_SUMMON = registerEntity("left_hand_witch_summon");
 
-    public static final Tag<Block> HEATS_CAULDRON = TagFactory.BLOCK.create(new Identifier(WK.MODID, "heats_cauldron"));
+    public static final TagKey<Block> HEATS_CAULDRON = registerBlock("heats_cauldron");
 
-    public static final Tag<Item> CAULDRON_BLACKLIST = TagFactory.ITEM.create(new Identifier(WK.MODID, "cauldron_blacklist"));
-    public static final Tag<Item> OVEN_BLACKLIST = TagFactory.ITEM.create(new Identifier(WK.MODID, "oven_blacklist"));
-    public static final Tag<Item> BARREL_BLACKLIST = TagFactory.ITEM.create(new Identifier(WK.MODID, "barrel_blacklist"));
-    public static final Tag<Item> TEA_BLACKLIST = TagFactory.ITEM.create(new Identifier(WK.MODID, "tea_blacklist"));
-    public static final Tag<Item> VALID_BREW_ITEM = TagFactory.ITEM.create(new Identifier(WK.MODID, "valid_brew_item"));
-    public static final Tag<Item> RESETS_CAULDRON = TagFactory.ITEM.create(new Identifier(WK.MODID, "resets_cauldron"));
+    public static final TagKey<Item> CAULDRON_BLACKLIST = registerItem("cauldron_blacklist");
+    public static final TagKey<Item> OVEN_BLACKLIST = registerItem("oven_blacklist");
+    public static final TagKey<Item> BARREL_BLACKLIST = registerItem("barrel_blacklist");
+    public static final TagKey<Item> TEA_BLACKLIST = registerItem("tea_blacklist");
+    public static final TagKey<Item> VALID_BREW_ITEM = registerItem("valid_brew_item");
+    public static final TagKey<Item> RESETS_CAULDRON = registerItem("resets_cauldron");
+
+    static {
+        if (WKConfig.get().debugMode) {
+            WK.logger.info("Witches Kitchen Base Custom Tags: Successfully Loaded");
+        }
+    }
 
     private static TagKey<EntityType<?>> registerEntity(String id) {
         return TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(id));
@@ -41,11 +46,5 @@ public class WKTags {
 
     private static TagKey<Block> registerBlock(String id) {
         return TagKey.of(Registry.BLOCK_KEY, new Identifier(id));
-    }
-
-    static {
-        if (WKConfig.get().debugMode) {
-            WK.logger.info("Witches Kitchen Base Custom Tags: Successfully Loaded");
-        }
     }
 }
