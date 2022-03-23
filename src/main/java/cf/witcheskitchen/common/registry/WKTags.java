5ce02b59233c89493edee957a0bb1133802f75ad
Sunconure11 <eyespot7b@aol.com>
2022-03-23 14:29:03 -0400

    Redoing tags again, now that I know how to do them proper

--- src/main/java/cf/witcheskitchen/common/registry/WKTags.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKTags.java
@@ -16,35 +16,23 @@ public class WKTags {
     public static final TagKey<EntityType<?>> TAGLOCK_BLACKLIST = registerEntity("taglock_blacklist");
     public static final TagKey<EntityType<?>> COLD_IRON_WEAK = registerEntity("cold_iron_weak");
     public static final TagKey<EntityType<?>> COLD_IRON_IMMUNE = registerEntity("cold_iron_immune");
-    public static final TagKey<EntityType<?>> SILVER_WEAK = registerEntity("silver_iron_weak");
-    public static final TagKey<EntityType<?>> SILVER_IMMUNE = registerEntity("silver_iron_immune");
-    public static final TagKey<EntityType<?>> RIGHT_HAND_WITCH_SUMMON = registerEntity("right_hand_witch_summon");
-    public static final TagKey<EntityType<?>> LEFT_HAND_WITCH_SUMMON = registerEntity("left_hand_witch_summon");
+    public static final TagKey<EntityType<?>> SILVER_WEAK = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(WK.MODID, "silver_weak"));
+    public static final TagKey<EntityType<?>> SILVER_IMMUNE = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(WK.MODID, "silver_immune"));
+    public static final TagKey<EntityType<?>> RIGHT_HAND_WITCH_SUMMON = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(WK.MODID, "right_hand_witch_summon"));
+    public static final TagKey<EntityType<?>> LEFT_HAND_WITCH_SUMMON = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(WK.MODID, "left_hand_witch_summon"));
 
-    public static final TagKey<Block> HEATS_CAULDRON = registerBlock("heats_cauldron");
+    public static final TagKey<Block> HEATS_CAULDRON = TagKey.of(Registry.BLOCK_KEY, new Identifier(WK.MODID, "heats_cauldron"));
 
-    public static final TagKey<Item> CAULDRON_BLACKLIST = registerItem("cauldron_blacklist");
-    public static final TagKey<Item> OVEN_BLACKLIST = registerItem("oven_blacklist");
-    public static final TagKey<Item> BARREL_BLACKLIST = registerItem("barrel_blacklist");
-    public static final TagKey<Item> TEA_BLACKLIST = registerItem("tea_blacklist");
-    public static final TagKey<Item> VALID_BREW_ITEM = registerItem("valid_brew_item");
-    public static final TagKey<Item> RESETS_CAULDRON = registerItem("resets_cauldron");
+    public static final TagKey<Item> CAULDRON_BLACKLIST = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "cauldron_blacklist"));
+    public static final TagKey<Item> OVEN_BLACKLIST = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "oven_blacklist"));
+    public static final TagKey<Item> BARREL_BLACKLIST = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "barrel_blacklist"));
+    public static final TagKey<Item> TEA_BLACKLIST = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "tea_blacklist"));
+    public static final TagKey<Item> VALID_BREW_ITEM = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "valid_brew_item"));
+    public static final TagKey<Item> RESETS_CAULDRON = TagKey.of(Registry.ITEM_KEY, new Identifier(WK.MODID, "resets_cauldron"));
 
     static {
         if (WKConfig.get().debugMode) {
             WK.logger.info("Witches Kitchen Base Custom Tags: Successfully Loaded");
         }
-    }
-
-    private static TagKey<EntityType<?>> registerEntity(String id) {
-        return TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(id));
-    }
-
-    private static TagKey<Item> registerItem(String id) {
-        return TagKey.of(Registry.ITEM_KEY, new Identifier(id));
-    }
-
-    private static TagKey<Block> registerBlock(String id) {
-        return TagKey.of(Registry.BLOCK_KEY, new Identifier(id));
     }
 }
