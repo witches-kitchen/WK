eb6be31357b9dbd963d0f7df0532628d359e46fe
= <danikingrd@gmail.com>
2022-05-24 13:45:40 -0400

    Rewrite sound events registry

--- src/main/java/cf/witcheskitchen/common/registry/WKSoundEvents.java
+++ src/main/java/cf/witcheskitchen/common/registry/WKSoundEvents.java
@@ -2,29 +2,29 @@ package cf.witcheskitchen.common.registry;
 
 import cf.witcheskitchen.WK;
 import cf.witcheskitchen.WKConfig;
+import cf.witcheskitchen.WKIdentifier;
+import cf.witcheskitchen.api.registry.ObjectDefinition;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WKSoundEvents {
-
-    private static final Map<Identifier, SoundEvent> SOUND_EVENTS = new HashMap<>();
+    public static final List<ObjectDefinition<SoundEvent>> SOUND_EVENTS = new ArrayList<>();
     // Cu-Sith 
     public static final SoundEvent CUSITH_IDLE_EVENT = register("cusith_ambient");
     public static final SoundEvent CUSITH_DEATH_EVENT = register("cusith_death");
     public static final SoundEvent CUSITH_HOWL_EVENT = register("cusith_howl");
-
     //Ferret
     public static final SoundEvent FERRET_IDLE_EVENT = register("ferret_ambient");
     public static final SoundEvent FERRET_ATTACK_EVENT = register("ferret_attack");
     public static final SoundEvent FERRET_CHIRP_EVENT = register("ferret_chirp");
-
     //Particles
     public static final SoundEvent BUBBLE = register("bubble");
-
     //Broom
     public static final SoundEvent BROOM_RIDING_EVENT = register("broom_loop");
     public static final SoundEvent BROOM_USE_EVENT = register("broom_mount1");
@@ -34,15 +34,14 @@ public class WKSoundEvents {
             WK.logger.info("Witches Kitchen Base Sounds: Successfully Loaded");
         }
     }
-
-    private static SoundEvent register(String name) {
-        Identifier id = new Identifier(WK.MODID, name);
-        SoundEvent soundEvent = new SoundEvent(id);
-        SOUND_EVENTS.put(id, soundEvent);
+    static SoundEvent register(String name) {
+        final Identifier id = new WKIdentifier(name);
+        final SoundEvent soundEvent = new SoundEvent(id);
+        final ObjectDefinition<SoundEvent> def = new ObjectDefinition<>(id, soundEvent);
+        SOUND_EVENTS.add(def);
         return soundEvent;
     }
-
     public static void register() {
-        SOUND_EVENTS.keySet().forEach(id -> Registry.register(Registry.SOUND_EVENT, id, SOUND_EVENTS.get(id)));
+        SOUND_EVENTS.forEach(entry -> Registry.register(Registry.SOUND_EVENT, entry.id(), entry.object()));
     }
 }
