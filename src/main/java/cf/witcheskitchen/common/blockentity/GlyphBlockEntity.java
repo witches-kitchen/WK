07b2e7eaec5db4e38029f1710173c218ebb41624
mrsterner <simon@sterner.dev>
2023-02-19 01:13:29 +0100

    more rewrite

--- src/main/java/cf/witcheskitchen/common/blockentity/GlyphBlockEntity.java
+++ src/main/java/cf/witcheskitchen/common/blockentity/GlyphBlockEntity.java
@@ -7,38 +7,6 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 
 public class GlyphBlockEntity extends WKBlockEntityWithInventory {
-    private static  final byte[][] small = {
-            {0, 1, 1, 1, 0},
-            {1, 0, 0, 0, 1},
-            {1, 0, 0, 0, 1},
-            {1, 0, 0, 0, 1},
-            {0, 1, 1, 1 ,0},
-    };
-    private static final byte[][] medium = {
-            {0, 0, 1, 1, 1, 0, 0},
-            {0, 1, 0, 0, 0, 1, 0},
-            {1, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 1},
-            {0, 1, 0, 0, 0, 1, 0},
-            {0, 0, 1, 1, 1, 0, 0}
-    };
-    private static final byte[][] large = {
-            {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
-            {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-            {0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-            {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-            {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0}
-    };
-
-
-
     public GlyphBlockEntity(BlockPos pos, BlockState state) {
         super(WKBlockEntityTypes.GLYPH, pos, state, 9);
     }
