3a54711a326e569c81ebb5c86bd26fd996b32c75
Anonymous <Sunconure11@users.noreply.github.com>
2023-03-09 11:07:34 -0500

    This is gonna need testing

--- src/main/java/cf/witcheskitchen/common/statuseffect/ReinforcementStatusEffect.java
+++ src/main/java/cf/witcheskitchen/common/statuseffect/ReinforcementStatusEffect.java
@@ -1,12 +1,15 @@
 package cf.witcheskitchen.common.statuseffect;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
 
 public class ReinforcementStatusEffect extends StatusEffect {
     public ReinforcementStatusEffect(StatusEffectType type, int color) {
         super(type, color);
+        this.addAttributeModifier(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, "e9b51c80-be93-11ed-afa1-0242ac120002", 2D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     @Override
