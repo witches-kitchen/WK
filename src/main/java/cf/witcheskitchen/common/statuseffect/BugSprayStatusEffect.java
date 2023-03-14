6b9a7b2c855a71933cf1692457b3573093b4aef9
Anonymous <Sunconure11@users.noreply.github.com>
2023-03-14 11:30:18 -0400

    Making room for bugspray potions

---
+++ src/main/java/cf/witcheskitchen/common/statuseffect/BugSprayStatusEffect.java
@@ -0,0 +1,16 @@
+package cf.witcheskitchen.common.statuseffect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+
+public class BugSprayStatusEffect extends StatusEffect {
+    protected BugSprayStatusEffect(StatusEffectType type, int color) {
+        super(type, color);
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+
+    }
+}
