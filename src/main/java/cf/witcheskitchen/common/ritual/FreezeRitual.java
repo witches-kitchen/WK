02c58d5f714b19bade21da9edb4c0bb2edbae308
Anonymous <Sunconure11@users.noreply.github.com>
2023-03-30 17:31:51 -0400

    WIP stuff on freeze ritual

--- src/main/java/cf/witcheskitchen/common/ritual/FreezeRitual.java
+++ src/main/java/cf/witcheskitchen/common/ritual/FreezeRitual.java
@@ -2,13 +2,18 @@ package cf.witcheskitchen.common.ritual;
 
 import cf.witcheskitchen.api.ritual.Ritual;
 import cf.witcheskitchen.common.recipe.RitualRecipe;
+import cf.witcheskitchen.common.registry.WKStatusEffects;
+import cf.witcheskitchen.common.statuseffect.ParalysisStatusEffect;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
 import java.util.List;
+
+import static cf.witcheskitchen.common.registry.WKStatusEffects.PARALYSIS;
 
 public class FreezeRitual extends Ritual {
     @Override
@@ -18,7 +23,7 @@ public class FreezeRitual extends Ritual {
         List<LivingEntity> list = world.getEntitiesByClass(LivingEntity.class, new Box(blockPos).expand(strength), livingEntity ->
                 livingEntity.squaredDistanceTo(blockPos.getX(), blockPos.getY(), blockPos.getZ()) < strength && !(livingEntity instanceof PlayerEntity));
         for (LivingEntity entity : list) {
-            //TODO disable movement
+            entity.setVelocity(0, 0, 0);
         }
 
     }
