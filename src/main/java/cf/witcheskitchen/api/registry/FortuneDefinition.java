6d0f2ec8c225a72b8fc88caf6c2606d5e4b8d35c
Anonymous <Sunconure11@users.noreply.github.com>
2025-09-17 18:27:59 -0400

    Publishing what I have for now

---
+++ src/main/java/cf/witcheskitchen/api/registry/FortuneDefinition.java
@@ -0,0 +1,57 @@
+package cf.witcheskitchen.api.registry;
+
+import cf.witcheskitchen.WitchesKitchen;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+
+public abstract class FortuneDefinition {
+
+
+    /**
+     * Is the fortune bad?
+     */
+    public boolean isNegative;
+
+    /**
+     * Does the fortune fire instantly?
+     */
+    public boolean isInstant;
+
+    /**
+     * How many ticks at minimum
+     * does it take for a non-instant
+     * fortune to fire?
+     */
+    public int minTimeFrame;
+
+    /**
+     * How many ticks at maximum
+     * does it take for a non-instant
+     * fortune to fire?
+     */
+    public int maxTimeFrame;
+
+    /**
+     * Check if the player is a valid
+     * target for the fortune
+     */
+    public boolean isValid (Player player) {
+        return true;
+    }
+
+    /**
+     * Apply the fortune
+     */
+    public abstract boolean apply(Player player);
+
+    /**
+     * Fortune definition
+     * Needs more work
+     */
+    public Fortune (ResourceLocation id, boolean isNegative, int minTimeFrame, int maxTimeFrame) {
+        this.isNegative = isNegative;
+        this.minTimeFrame = minTimeFrame;
+        this.maxTimeFrame = maxTimeFrame;
+    }
+
+}
